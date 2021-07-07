d3131dbdceebf5b6d04e6c3a10cce7f4350eb728
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-07-07 13:48:40 -0400

    Complete assignment v.01

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(ary)
+    returns = []
+    sorted = @word.split("").sort
+    ary.each do |et|
+      returns << et if et.split("").sort == sorted
+    end
+    returns
+  end
+end
